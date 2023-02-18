8103c10ab02d49f12e7c733e2c69a9886cfd41b0
3rick <erick.dejesus1@alumno.buap.mx>
2023-02-17 18:37:27 -0600

    Define functions: parse, route, format
    parse a request, returning maps
    %{}

--- lib/traefik/handler.ex
+++ lib/traefik/handler.ex
@@ -5,6 +5,33 @@ defmodule Traefik.Handler do
     |> route()
     |> format_response()
   end
+
+  def parse(request) do
+    %{
+      method: "GET",
+      path: "/hello",
+      response: ""
+    }
+  end
+
+  def route(conn) do
+    %{
+      method: "GET",
+      path: "/hello",
+      esponse: "Hello world"
+    }
+  end
+
+    def format_response(conn) do
+      """
+      HTTP/1.1 200 OK
+      Host: some.com
+      User-Agent: telnet
+      Accept: */*
+
+      Hello world
+      """
+    end
 end
 
 request = """
