725f44bce46678249678fe44cb895d24d0af535c
3rick <erick.dejesus1@alumno.buap.mx>
2023-03-01 23:00:18 -0600

    Add the controller for handle API request

---
+++ lib/traefik/api/developer_controller.ex
@@ -0,0 +1,10 @@
+defmodule Traefik.Api.DeveloperController do
+  alias Traefik.Conn
+
+  def index(%Conn{} = conn, _params \\ %{}) do
+    json =
+      Traefik.Organization.list_developers(%{limit: 10, offset: 0})
+      |> Jason.encode!()
+    %{conn | status: 200, response: json, content_type: "appplication/json"}
+  end
+end
