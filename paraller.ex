6e6dc06c2305716944608ef54c8571b59024b978
3rick <erick.dejesus1@alumno.buap.mx>
2023-03-05 19:45:10 -0600

    Parallel map

---
+++ paraller.ex
@@ -0,0 +1,19 @@
+defmodule Paraller do
+  def pmap(collection, fun) do
+    collection
+    |> Enum.map(&spawn_process(&1, self(), fun))
+    |> Enum.map(&await/1)
+  end
+
+  defp spawn_process(n, parent, fun) do
+    spawn(fn ->
+      send(parent, {self(), fun.(n)})
+    end)
+  end
+
+  defp await(pid) do
+    receive do
+      {^pid, result} -> result
+    end
+  end
+end
